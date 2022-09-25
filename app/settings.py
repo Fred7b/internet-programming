6c8825cfa9d186df105caa5ef73c0fd633ad0ef0
FedorPudovkin <Fedor.Pudovkin@krontech.ru>
2022-09-25 19:10:16 +0400

    Добавил классы и методы запросов к сервисы

--- app/settings.py
+++ app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'task_manager',
 ]
 
@@ -126,3 +127,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+PAGINATION_POSTS_PER_PAGE = 5
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,28 +1,67 @@
 from django.shortcuts import render
 
-
 # Create your views here.
+from django.utils import timezone
+from rest_framework.generics import CreateAPIView, UpdateAPIView, RetrieveUpdateAPIView
+
+from task_manager.models import Task, Project
+from task_manager.serializers import CreateTaskSerializer, UpdateTaskSerializer, ChangeStatusTaskSerializer, \
+    CreateProjectSerializer, UpdateProjectSerializer
+from task_manager.services import servises
+
+
+class CreateTaskAPIView(CreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = CreateTaskSerializer
+
+    def perform_create(self, serializer):
+        # pass
+        serializer.save(created_by=self.request.user,
+                        created_at=timezone.now())
+
+
+class UpdateTaskAPIView(RetrieveUpdateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = UpdateTaskSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        serializer.save(last_modified=timezone.now())
+
+
+class CreateProjectAPIView(CreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = CreateProjectSerializer
+
+    def perform_create(self, serializer):
+        # pass
+        serializer.save(created_at=timezone.now())
+
+
+class UpdateProjectAPIView(RetrieveUpdateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = UpdateProjectSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        serializer.save(last_modified=timezone.now())
+
+class ChangeStatusTaskAPIView(RetrieveUpdateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = ChangeStatusTaskSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        serializer.save(last_modified=timezone.now())
 
 
 def task_list(request):
-    data = {}
+    tasks = servises.get_tasks(request)
+    data = {'tasks': tasks}
     return render(request, 'task_list.html', data)
 
 
-def task_detail(request):
-    data = {}
+def task_detail(request, pk):
+    task = servises.get_task_by_pk(pk)
+    data = {'task': task}
     return render(request, 'task_detail.html', data)
-
-
-def task_create(request):
-    data = {}
-    return render(request, 'task_create.html', data)
-
-
-def task_update(request):
-    data = {}
-    return render(request, 'task_update.html', data)
-
-
-def task_change_status(request):
-    pass
